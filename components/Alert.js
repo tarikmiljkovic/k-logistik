a70e8121c67e9d6e8c10bebec1c8f4450e3c1902
Miljkovic <tarik.miljkovic@stud.fh-campuswien.ac.at>
2021-06-03 17:42:07 +0200

    awd

--- components/Alert.js
+++ components/Alert.js
@@ -9,30 +9,33 @@ import {
   Alert,
 } from "theme-ui";
 
+import { MdPhone, MdMailOutline } from "react-icons/md";
+
 export default function MyAlert() {
 
   return (
     <>
       <Alert>
         <Container sx={{ px: [0, 4, 4] }}>
-          <Flex sx={{ justifyContent: "space-between", flexWrap: "wrap" }}>
-            <Box sx={{ flex: "0 1 auto" }}>
-              <div>
-                tel:{" "}
-                <Link href="tel:+4915203468634">
-                  <a sx={{ variant: "styles.alertnavlink" }}>+49 15203468634</a>
-                </Link>
-              </div>
-            </Box>
-            <Box>
-              <div>
-                {" "}
-                mail:
-                <Link href="mailto:info@karajic.de">
-                  <a sx={{ variant: "styles.alertnavlink" }}>info@karajic.de</a>
-                </Link>
-              </div>
-            </Box>
+          <Flex
+            sx={{
+              justifyContent: "space-between",
+              flexWrap: "wrap",
+              flexItems: "center",
+            }}
+          >
+            <Flex sx={{ flex: "0 1 auto" }}>
+              <MdPhone sx={{ height: "100%" }} />
+              <Link href="tel:+4915203468634">
+                <a sx={{ variant: "styles.alertnavlink" }}>+49 15203468634</a>
+              </Link>
+            </Flex>
+            <Flex>
+              <MdMailOutline sx={{ height: "100%" }} />
+              <Link href="mailto:info@karajic.de">
+                <a sx={{ variant: "styles.alertnavlink" }}>info@karajic.de</a>
+              </Link>
+            </Flex>
           </Flex>
         </Container>
       </Alert>
